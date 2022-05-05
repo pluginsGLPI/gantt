bb8089ca2e9c152757c0286ba262cdd4ccedf8c9
adelaunay <delaunay.alexandre@gmail.com>
2022-05-05 14:13:47 +0200

    clean

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,3 @@
-/**
- * -------------------------------------------------------------------------
- * advanceddashboard plugin for GLPI
- * -------------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of advanceddashboard.
- *
- * advanceddashboard is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * advanceddashboard is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with advanceddashboard. If not, see <http://www.gnu.org/licenses/>.
- * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2020-2022 by Teclib'.
- * @license   GPLv3 https://www.gnu.org/licenses/gpl-3.0.html
- * @link      https://services.glpi-network.com
- * -------------------------------------------------------------------------
- */
-
 const webpack = require('webpack');
 const path = require('path');
 
