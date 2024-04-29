2bef84c3a2daea11d875962701dee30642c23740
Rom1-B <rbiot@teclib.com>
2024-04-29 09:29:14 +0200

    public/lib

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = [
       },
       output: {
          filename: 'libs.js',
-         path: path.resolve(__dirname, 'public/build'),
+         path: path.resolve(__dirname, 'public/lib'),
       },
       module: {
          rules: [
