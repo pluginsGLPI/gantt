1046c1c16412fe839e1e55a498e6e715f2f93dd0
Rom1-B <rom1.biot@gmail.com>
2025-06-23 10:25:10 +0200

    Fix: JS build patch

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = [
       },
       output: {
          filename: 'libs.js',
-         path: path.resolve(__dirname, 'public/lib'),
+         path: path.resolve(__dirname, 'public/build'),
       },
       module: {
          rules: [
